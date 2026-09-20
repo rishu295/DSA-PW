dbde6db53d8836b4f4b78001b05174fb4d5cdcb4
Rishu <rishunishad1308@gmail.com>
2026-09-21 04:05:43 +0530

    leetcode 121 done after various approaches

--- lc-121.cpp
+++ lc-121.cpp
@@ -3,5 +3,21 @@ using namespace std;
 int main(){
     int prices[] = {7,1,5,3,6,4};
     int n = sizeof(prices)/sizeof(prices[0]);
-     
+    int current = 0;
+    int low = prices[0];
+    int max_profit = 0;
+    int profit = 0;
+    for(int i=0; i<n; i++){
+        current = prices[i];
+        if(current<low){
+            low = current;
+        }
+        if(current>low){
+            profit = current - low;
+        }
+        if(profit>max_profit){
+            max_profit = profit;
+        }
+    }
+    cout<<max_profit;
 }
